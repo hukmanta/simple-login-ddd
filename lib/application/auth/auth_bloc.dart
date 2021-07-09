50ae72bbfdf9bba96ff7c061dd27f822999684a5
hukman <hukmanta@gmail.com>
2021-07-09 15:59:54 +0700

    9(Fix). Navigation base on Auth State

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:simple_login_ddd/domain/weather/i_weather_facade.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
@@ -11,19 +12,20 @@ part 'auth_bloc.freezed.dart';
 
 @injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(const AuthState.initial());
+  final IWeatherFacade _iWeatherFacade;
+  AuthBloc(this._iWeatherFacade) : super(const AuthState.initial());
 
   @override
   Stream<AuthState> mapEventToState(
     AuthEvent event,
   ) async* {
-    event.map(
-      authCheckRequested: (e)  {
-        const AuthState.unauthenticated();
-      }
-      /*grantedLogIn: (e) {
-        const AuthState.authenticated();
-      },*/
+    yield* event.map(grantedLogIn: (GrantedLogin value) async* {
+        yield const AuthState.authenticated();
+    },
+        authCheckRequested: (AuthCheckRequested value) async* {
+        yield const AuthState.unauthenticated();
+        }
+
     );
   }
 }
